e7cb1e9ad60f58fa3873537e864b090b023b2715
Paul <paul.gladoon@gmail.com>
2016-05-27 12:45:16 +0300

    Create test.js

---
+++ papka/test.js
@@ -0,0 +1,7 @@
+$(function(){
+    $('#nav li').hover(function(){
+        $(this).children('ul').stop(false, true).fadeIn(300);
+    },function(){
+        $(this).children('ul').stop(false, true).fadeOut(300);
+    });
+});
