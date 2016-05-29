f8f33e5cd71c8643e1ce0a3bf60c5a731d933f49
Paul <paul.gladoon@gmail.com>
2016-05-29 20:12:32 +0300

    Add files via upload

---
+++ projects/MyShablon/js/shablon.js
@@ -0,0 +1,29 @@
+$(function() {
+
+	var html = $('#test').html();
+	var info = { 
+	
+	  	name: 'Гладун Павел Николаевич',
+	  	photo: {
+	  		src: 'img/pavel.jpg',
+	  		alt: 'pasha'
+	  	},
+	  	about: 'Вольный художник импресыссионист, мечтающий о постоянной работе.',
+	  	why: 'Хочу учить frontend, потому что:',
+	  	because: ['Кисти пересохли',
+	  	          'Мальберты дорогие',
+	  	          'Работа 24 часа в сутки'],
+	  	tel: 'Мой контактный телефон:',
+	  	number: '380631556565',
+	  	vk: 'Мой профиль Вконтакте:',
+	  	vkLink: 'https://vk.com/paul.gladoon',
+	  	feedback: 'Мой feedback:',
+	  	feedbackText: 'могу работать за еду.'
+	};
+	  
+
+	var content = tmpl(html, info);
+
+
+	$('body').append(content);
+});
