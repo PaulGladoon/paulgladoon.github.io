e9071239ebbd40685e54608945001d551ef411c7
Paul <paul.gladoon@gmail.com>
2016-07-01 17:07:42 +0300

    Add files via upload

---
+++ projects/BuildHome/app/js/common.js
@@ -0,0 +1,7 @@
+$(function() {
+    $( "#accordion" ).accordion();
+    $( "#accordion" ).accordion({
+      icons: { "header": "ui-icon-plus", "activeHeader": "ui-icon-minus" }
+    });
+    $('.bxslider').bxSlider();
+});
