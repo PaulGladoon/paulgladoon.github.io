6ee1de58bf6eec1e243dcd12d69fbef70460c5cc
Paul <paul.gladoon@gmail.com>
2016-07-22 11:55:34 +0300

    Add files via upload

--- projects/Test/gulpfile.js
+++ projects/Test/gulpfile.js
@@ -25,7 +25,7 @@ var path = {
     src: {
         html: 'src/*.html',
         js: 'src/js/*.js',
-        style: 'src/sass/main.scss',
+        style: ['src/sass/main.scss', 'src/scss/ie8.scss'],
         img: 'src/img/**/*.*',
         fonts: 'src/fonts/**/*.*'
     },
