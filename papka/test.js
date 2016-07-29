eb1176c0fba1580844f0d72bd30f7a2c2290a990
Pasha G <paul.gladoon@gmail.com>
2016-07-29 13:23:30 +0300

    Delete papka, rename

--- papka/test.js
+++
@@ -1,7 +0,0 @@
-$(function(){
-    $('#nav li').hover(function(){
-        $(this).children('ul').stop(false, true).fadeIn(300);
-    },function(){
-        $(this).children('ul').stop(false, true).fadeOut(300);
-    });
-});
