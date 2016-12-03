708af48ba026b831a658a85ba186b4fa3b9a8072
Paul <paul.gladoon@gmail.com>
2016-12-03 19:26:46 +0200

    Update goitautotest.js

--- projects/9 Autotests JS/goitautotest.js
+++ projects/9 Autotests JS/goitautotest.js
@@ -1,10 +1,23 @@
 var webdriver = require('selenium-webdriver');
-var driver = new webdriver.Builder().forBrowser('chrome').build();
+var driver = new webdriver.Builder().forBrowser('chrome').build()
 
-driver.get('http://google.com');
-driver.findElement(webdriver.By.name('q')).sendKeys('GO it qa Украина');
-driver.findElement(webdriver.By.className('sbico')).click();
-driver.wait(webdriver.until.elementLocated({ xpath: '//*[@id="rso"]/div/div/div[1]/div/h3/a' }), 1000);
-driver.findElement({ xpath: '//*[@id="rso"]/div/div/div[1]/div/h3/a' }).click();
-driver.getTitle().then(function(title) { console.log(title); });
-driver.quit();
+driver.get('http://google.com').then(function() {
+  console.log('1.Get to google.com --> PASSED')
+});
+driver.findElement(webdriver.By.name('q')).sendKeys('GO it qa Украина').then(function() {
+  console.log('2.Enter the Keys --> PASSED')
+});
+driver.findElement(webdriver.By.className('sbico')).click().then(function() {
+  console.log('3.Click on search btn --> PASSED')
+});
+driver.wait(webdriver.until.elementLocated({ xpath: '//*[@id="rso"]/div/div/div[1]/div/h3/a' }), 2000);
+driver.findElement({ xpath: '//*[@id="rso"]/div/div/div[1]/div/h3/a' }).click().then(function() {
+  console.log('4.Click first link --> PASSED')
+});
+driver.getTitle().then(function(title) {
+  console.log('5.Title of webpage --> PASSED');
+  console.log(title);
+});
+driver.quit().then(function() {
+  console.log('END TEST')
+});
