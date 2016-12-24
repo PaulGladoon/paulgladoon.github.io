e47df810d17af13c26808680588f0435bf949c22
Paul <paul.gladoon@gmail.com>
2016-12-24 22:50:40 +0200

    Update goitautotest.js

--- projects/8 Autotests JS/goitautotest.js
+++ projects/8 Autotests JS/goitautotest.js
@@ -1,10 +1,25 @@
 var webdriver = require('selenium-webdriver');
-var driver = new webdriver.Builder().forBrowser('chrome').build();
+var assert = require('assert');
+var driver = new webdriver.Builder().forBrowser('chrome').build()
 
-driver.get('http://google.com');
-driver.findElement(webdriver.By.name('q')).sendKeys('GO it qa Украина');
-driver.findElement(webdriver.By.className('sbico')).click();
-driver.wait(webdriver.until.elementLocated({ xpath: '//*[@id="rso"]/div/div/div[1]/div/h3/a' }), 1000);
-driver.findElement({ xpath: '//*[@id="rso"]/div/div/div[1]/div/h3/a' }).click();
-driver.getTitle().then(function(title) { console.log(title); });
-driver.quit();
+driver.get('http://google.com').then(function() {
+  console.log('1.Get to google.com --> PASSED')
+});
+driver.findElement(webdriver.By.name('q')).sendKeys('GO it qa Украина').then(function() {
+  console.log('2.Enter the Keys --> PASSED')
+});
+driver.findElement(webdriver.By.className('sbico')).click().then(function() {
+  console.log('3.Click on btn --> PASSED')
+});
+driver.wait(webdriver.until.elementLocated({ xpath: '//*[@id="rso"]/div/div/div[1]/div/h3/a' }), 2000);
+driver.findElement({ xpath: '//*[@id="rso"]/div/div/div[1]/div/h3/a' }).click().then(function() {
+  console.log('4.Click first link --> PASSED')
+});
+driver.getTitle().then(function(title) {
+    var expected = 'GoIT — лучшие IT курсы в Украине! | GoIT.ua';
+    assert.equal(expected,title, 'FAILED --> Title not equal');
+  });
+driver.quit().then(function() {
+  console.log('END TEST')
+});
+
