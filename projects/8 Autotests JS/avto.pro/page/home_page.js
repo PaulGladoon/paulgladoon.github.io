71d24503db6c4f5144135daaad0fedce1605f5d7
Paul <paul.gladoon@gmail.com>
2017-01-16 18:08:50 +0200

    Update home_page.js

--- projects/8 Autotests JS/avto.pro/page/home_page.js
+++ projects/8 Autotests JS/avto.pro/page/home_page.js
@@ -1,32 +1,40 @@
 var home_page = function() {
 
-  this.enterLinkClick = function() {
+  this.enterLinkClick = function() { // клик на ссылку "Вход" вверху формы
     element(by.xpath('//*[@id="secondary_sections"]/div/div[1]/ul/li[1]/a')).click();
   }
 
-  this.regLinkClick = function() {
+  this.regLinkClick = function() { // клик на ссылку "Регистрация" вверху формы
     element(by.css('[data-target="#sign-up-form-container"]')).click();
   };
 
-  this.enterEmailFieldValue = function(value) {
+  this.enterEmailFieldValue = function(value) { // ввод в поле "Email"
     element(by.xpath('//*[@id="sign-up-form-container"]/div/form/label[1]/div[1]/input')).sendKeys(value);
   };
 
-  this.emailField = function() {
+  this.emailField = function() { // поле "Email"
     return element(by.xpath('//*[@id="sign-up-form-container"]/div/form/label[1]/div[1]/input'));
   }
 
-  this.enterPasswordFieldValue = function(value) {
+  this.enterPasswordFieldValue = function(value) { // ввод в поле "Password"
     element(by.xpath('//*[@id="sign-up-form-container"]/div/form/label[2]/div[1]/input')).sendKeys(value);
   };
 
-  this.passwordField = function() {
+  this.passwordField = function() { // поле "Password"
     return element(by.xpath('//*[@id="sign-up-form-container"]/div/form/label[2]/div[1]/input'));
   }
 
-  this.clickOnRegButton = function() {
+  this.getPasswordFieldAttr = function(value) { // взять атрибут из поля "Password"
+    return element(by.xpath('//*[@id="sign-up-form-container"]/div/form/label[2]/div[1]/input')).getAttribute(value);
+  }
+
+  this.clickOnRegButton = function() { // клик на кнопку "Регистрация"
     element(by.xpath('//*[@id="sign-up-form-container"]/div/form/div/button')).click();
   };
+
+  this.clickOnEyeOff = function() { // клик на action копку "Глаз" откл.
+    element(by.xpath('//*[@id="sign-up-form-container"]/div/form/label[2]/div[1]/button/i')).click();
+  }
 
 
 };
