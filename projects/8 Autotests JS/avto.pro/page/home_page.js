52297cbbf9cab4e34622877d5b9d054069c53915
Paul <paul.gladoon@gmail.com>
2017-01-16 13:10:50 +0200

    Update home_page

--- projects/8 Autotests JS/avto.pro/page/home_page.js
+++ projects/8 Autotests JS/avto.pro/page/home_page.js
@@ -1,1 +1,26 @@
+var home_page = function() {
+
+  this.regLinkClick = function() {
+    element(by.css('[data-target="#sign-up-form-container"]')).click();
+  };
+
+  this.enterEmailFieldValue = function(value) {
+    element(by.xpath('//*[@id="sign-up-form-container"]/div/form/label[1]/div[1]/input')).sendKeys(value);
+  };
+
+  this.enterPasswordFieldValue = function(value) {
+    element(by.xpath('//*[@id="sign-up-form-container"]/div/form/label[2]/div[1]/input')).sendKeys(value);
+  };
+
+  this.clickOnRegButton = function() {
+    element(by.xpath('//*[@id="sign-up-form-container"]/div/form/div/button')).click();
+  };
+
+  this.profileHrefFind = function() {
+    return element(by.xpath('//*[@id="secondary_sections"]/div/nav/div/div/ul[2]/li[2]/a')).getAttribute('href')
+  };
+
+};
+
+module.exports = new home_page();
 
