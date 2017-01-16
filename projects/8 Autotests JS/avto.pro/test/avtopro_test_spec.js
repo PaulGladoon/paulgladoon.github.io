541a94cff2ffaefecfd598c4aa1a7cbf69d092fe
Paul <paul.gladoon@gmail.com>
2017-01-16 14:43:43 +0200

    Update avtopro_test_spec.js

--- projects/8 Autotests JS/avto.pro/test/avtopro_test_spec.js
+++ projects/8 Autotests JS/avto.pro/test/avtopro_test_spec.js
@@ -1,4 +1,5 @@
 var home_page = require('../page/home_page.js');
+var profile_page = require('../page/profile_page.js')
 
 var EC = protractor.ExpectedConditions;
 
@@ -6,7 +7,10 @@ describe ('Positive testing of registration form', function() {
   beforeEach(function () {
     browser.ignoreSynchronization = true;
     browser.get('https://avto.pro/');
-    // browser.close();
+  });
+
+  afterEach(function () {
+    browser.restart();
   });
 
   it ('Registration of the user with valid data', function() {
@@ -14,11 +18,23 @@ describe ('Positive testing of registration form', function() {
     home_page.enterEmailFieldValue('autotest@gm.com');
     home_page.enterPasswordFieldValue('hello123');
     home_page.clickOnRegButton();
-    browser.sleep(2000);
+    browser.sleep(3000);
     var profileHref = home_page.profileHrefFind()
+
+    expect(profileHref).toBe('https://avto.pro/users/profile/195776/personal/');
+  });
+
+  it ('Registration confirmations by means of buttons by default', function() {
+    home_page.regLinkClick();
+    home_page.enterEmailFieldValue('autotest@gm.com');
+    home_page.emailField().sendKeys(protractor.Key.TAB);
+    home_page.enterPasswordFieldValue('hello123');
+    home_page.passwordField().sendKeys(protractor.Key.ENTER);
+    browser.sleep(3000)
+    var profileHref = home_page.profileHrefFind()
+
     expect(profileHref).toBe('https://avto.pro/users/profile/195776/personal/');
   });
 
 
 });
-
